7b08d4b4dd08222032d690b5dc153af3dba92b82
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-06-25 09:58:10 -0600

    Modify css and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import "./App.css";
+import styles from "./App.module.css";
 
-function App() {
+export default function App() {
   return (
     <>
-      <h1>Hello Gabe!</h1>
+      <header className={styles.header}>
+        <h1>Hello Gabe!</h1>
+        <nav className={styles.nav}>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <h2>Items</h2>
+          <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+            <li>Item 5</li>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" />
+            </div>
+          </ul>
+        </section>
+      </main>
     </>
   );
 }
-
-export default App;
